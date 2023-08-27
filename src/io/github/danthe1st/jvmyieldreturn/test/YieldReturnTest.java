32e0d5d7a5606e815711b4dfcaf5d3438b51a3a8
danthe1st <daniel@wwwmaster.at>
2023-08-27 22:30:21 +0200

    combine declaration and assignment

--- src/io/github/danthe1st/jvmyieldreturn/test/YieldReturnTest.java
+++ src/io/github/danthe1st/jvmyieldreturn/test/YieldReturnTest.java
@@ -5,11 +5,9 @@ import io.github.danthe1st.jvmyieldreturn.Yielder;
 public class YieldReturnTest {
 	// --enable-preview --add-exports java.base/jdk.internal.vm=JVMYieldReturn
 	public static void main(String[] args) {
-		Iterable<String> it = Yielder.create(YieldReturnTest::someMethod);
-
 		System.out.println("main thread: " + Thread.currentThread());
 
-		for (String s : it) {
+		for (String s : Yielder.create(YieldReturnTest::someMethod)) {
 			System.out.println("Text: " + s);
 		}
 	}
