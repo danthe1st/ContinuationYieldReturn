7e3a0ebbce0c2e059328556a2507c94fc096ffbc
danthe1st <daniel@wwwmaster.at>
2023-09-05 21:14:59 +0200

    improve documentation with examples

--- src/io/github/danthe1st/jvmyieldreturn/Yielder.java
+++ src/io/github/danthe1st/jvmyieldreturn/Yielder.java
@@ -11,16 +11,31 @@ import jdk.internal.vm.ContinuationScope;
 
 /**
  * Allows to create an {@link Iterable} or {@link Stream} obtaining its values by lazily evaluating a function.
- *
+ * <p>
  * This function can send values to the {@link Iterable} by calling {@link Yielder#yield(Object) yield}.
  * When the first object is requested by an {@link Iterator}, the function is executed until a {@link Yielder#yield(Object) yield} call.
  * The function is then suspended until the next element is requested.
- *
+ * <p>
  * The provided function is called once per call to {@link Iterable#iterator()}.
- *
+ * <p>
+ * <b>The method {@link Yielder#yield(Object)} might return in a different thread or might not return at all. {@code try}-{@code finally} or closing of try-with-resources-blocks might not be executed if a {@link Yielder#yield(Object)} call happens inside these blocks.</b>
  * {@snippet
  * 	class="io.github.danthe1st.jvmyieldreturn.test.YieldReturnTest"
  * 	region="example"
+ * 	lang="java"
+ * }
+ * <p>
+ * In general, the {@link Yielder} object passed to the function should not be passed to other methods that might call {@link Yielder#yield(Object)} inside a {@code try}-{@code finally} or closing of try-with-resources-block.
+ * {@snippet
+ * 	class="io.github.danthe1st.jvmyieldreturn.test.YieldReturnTest"
+ * 	region="badCodeTryFinally"
+ * 	lang="java"
+ * }
+ * <p>
+ * In case a {@code try}-{@code finally} or closing of try-with-resources-block is needed, it can surround the complete iteration code ensuring that the resource is closed when the iteration completes.
+ * {@snippet
+ * 	class="io.github.danthe1st.jvmyieldreturn.test.YieldReturnTest"
+ * 	region="goodCodeTryFinally"
  * 	lang="java"
  * }
  * @param <T> The type of elements to iterate over.
@@ -36,7 +51,7 @@ public class Yielder<T> {
 	
 	/**
 	 * Passes an object to the corresponding {@link Iterator} and suspends until the next object is requested.
-	 *
+	 * <p>
 	 * <b>This method might return in a different thread or might not return at all. {@code try}-{@code finally} or closing of try-with-resources-blocks might not be executed if a {@link Yielder#yield(Object)} call happens inside these blocks.</b>
 	 * @param value The object that should be passed to the {@link Iterator}.
 	 */
@@ -47,7 +62,7 @@ public class Yielder<T> {
 	
 	/**
 	 * Creates an {@link Iterable} obtaining its values by lazily evaluating a function.
-	 *
+	 * <p>
 	 * <b>When calling {@link Yielder#yield(Object)} in the passed function, that call  might return in a different thread or might not return at all. {@code try}-{@code finally} or closing of try-with-resources-blocks might not be executed if a {@link Yielder#yield(Object)} call happens inside these blocks.</b>
 	 * @param <T> The type of elements to iterate over.
 	 * @param fun The function used for obtaining values to iterate over. Values are passed to the {@link Iterator} using a parameter passed to the function.
@@ -68,7 +83,7 @@ public class Yielder<T> {
 	
 	/**
 	 * Creates a {@link Stream} obtaining its values by lazily evaluating a function.
-	 *
+	 * <p>
 	 * <b>When calling {@link Yielder#yield(Object)} in the passed function, that call  might return in a different thread or might not return at all. {@code try}-{@code finally} or closing of try-with-resources-blocks might not be executed if a {@link Yielder#yield(Object)} call happens inside these blocks.</b>
 	 * @param <T> The type of the elements to iterate over.
 	 * @param fun The function used for obtaining values to iterate over. Values are passed to the {@link Stream} using a parameter passed to the function.
--- src/io/github/danthe1st/jvmyieldreturn/test/YieldReturnTest.java
+++ src/io/github/danthe1st/jvmyieldreturn/test/YieldReturnTest.java
@@ -4,36 +4,84 @@ import io.github.danthe1st.jvmyieldreturn.Yielder;
 
 class YieldReturnTest {
 	// --enable-preview --add-exports java.base/jdk.internal.vm=YieldReturn
-	// @start region="example"
 	public static void main(String[] args) {
+		YieldReturnTest test = new YieldReturnTest();
+		System.out.println("EXAMPLE CODE");
+		test.example();
+		System.out.println();
+		
+		System.out.println("==========================");
+		System.out.println("BAD CODE WITH try-finally");
+		test.badCodeTryFinally();
+		System.out.println();
+		
+		System.out.println("==========================");
+		System.out.println("GOOD CODE WITH try-finally");
+		test.goodCodeTryFinally();
+	}
+	// @start region="example"
+	private void example() {
 		System.out.println("main thread: " + Thread.currentThread());
 		
-		for (String s : Yielder.iterable(YieldReturnTest::someMethod)) {
+		for(String s : Yielder.iterable(this::someMethod)){
 			System.out.println("Text: " + s);
 		}
-
+		
 		System.out.println();
 		System.out.println("Now using streams:");
 		
-		Yielder.stream(YieldReturnTest::someMethod).limit(2).forEach(System.out::println);
+		Yielder.stream(this::someMethod).limit(2).forEach(System.out::println);
 	}
 	
-	private static void someMethod(Yielder<String> y) {
+	private void someMethod(Yielder<String> y) {
 		y.yield("Hello - " + Thread.currentThread());
 		System.out.println("between yields");
 		y.yield("World - " + Thread.currentThread());
 		
-		for (String s : Yielder.iterable(YieldReturnTest::otherMethod)) {
+		for(String s : Yielder.iterable(this::otherMethod)){
 			y.yield("nested: " + s);
 		}
 		
 		y.yield("bye - " + Thread.currentThread());
 	}
 	
-	private static void otherMethod(Yielder<String> y) {
+	private void otherMethod(Yielder<String> y) {
 		y.yield("it can");
 		y.yield("also be");
 		y.yield("nested");
+	}
+	// @end
+	
+	// @start region = "badCodeTryFinally"
+	private void badCodeTryFinally() {
+		Iterable<String> iterable = Yielder.iterable(this::methodWithTryFinally);
+		String firstElement = iterable.iterator().next();
+		System.out.println(firstElement);
+	}
+	
+	private void methodWithTryFinally(Yielder<String> y) {
+		try{
+			y.yield("Hello World");// DON'T DO THIS
+		}finally{// may not get executed
+			System.out.println("Some very important cleanup code");
+		}
+	}
+	// @end
+	
+	// @start region = "goodCodeTryFinally"
+	private void goodCodeTryFinally() {
+		try{
+			Iterable<String> iterable = Yielder.iterable(this::methodWithTryFinally);
+			String firstElement = iterable.iterator().next();
+			System.out.println(firstElement);
+		}finally{// will get executed
+			System.out.println("Some very important cleanup code");
+		}
+		
+	}
+	
+	private void yieldSomething(Yielder<String> y) {
+		y.yield("Hello World");// OK, this is not surrounded by any try-finally
 	}
 	// @end
 }
